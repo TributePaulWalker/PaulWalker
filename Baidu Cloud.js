b679107dba9d70ed9b40e386a7525e2a6bec3b46
Brian <hexiaoyu27149@sina.cn>
2021-03-29 11:45:49 +0800

    Update Baidu Cloud.js

--- Baidu Cloud.js
+++ Baidu Cloud.js
@@ -1,4 +1,16 @@
 //修改自https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+QuantumultX
+[rewrite_local]
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/TributePaulWalker/Scripts/main/Baidu%20Cloud.js
+
+Surge 
+[Script]
+
+百度云VIP倍速播放 = type=http-response,pattern=https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user,requires-body=1,max-size=0,timeout=20,script-path=https://raw.githubusercontent.com/TributePaulWalker/Scripts/main/Baidu%20Cloud.js
+
+[MITM]
+hostname = pan.baidu.com
+
 let obj = {
   "product_infos": [{
     "product_id": "5310897792128633390",
