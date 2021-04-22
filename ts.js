518010149757eba7e692e2aa21fd91a41db080a0
Brian <hexiaoyu27149@sina.cn>
2021-04-23 02:47:59 +0800

    Update ts.js

--- ts.js
+++ ts.js
@@ -30,7 +30,7 @@ Sub_info = type=http-request,pattern=http://sub\.info,script-path=https://raw.gi
   let infoList = [`使用: ${used} | ${total}`];
   
   if (resetLeft) {
-    infoList.push(`重置: ${resetLeft} Day${resetLeft == 1 ? "" : "s"}`);
+    infoList.push(`重置: ${resetLeft} 天${resetLeft == 1 ? "" : "s"}`);
   }
   if (expire) {
     if (/^[\d]+$/.test(expire)) {
